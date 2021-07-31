4798a33674f5bd6e4e328d8194c828a99ec63a80
Ronnie Song <ronniesong0809@gmail.com>
2021-07-31 15:01:16 -0700

    change favicon, marker, and icon

--- src/components/Marker.js
+++ src/components/Marker.js
@@ -4,9 +4,9 @@ const Marker = ({ lat, lng, onClick }) => {
   return (
     <div className="marker" onClick={onClick}>
       <Icon
-        icon="ant-design:fire-twotone"
         className="icon"
-        style={{ transform: "translate(-50%, -50%)" }}
+        icon="ant-design:environment-twotone"
+        style={{ transform: "translate(-50%, -100%)" }}
       />
     </div>
   );
--- src/components/MessageBox.js
+++ src/components/MessageBox.js
@@ -1,6 +1,9 @@
+import { ReactComponent as Wildfire } from "../assets/wildfire.svg";
+
 const MessageBox = ({ info }) => {
   return (
     <div className="messageBox absolute bottom-8 left-8 z-50 p-3 w-1/4 bg-gray-800 text-gray-200 opacity-80 rounded-2xl shadow-2xl">
+      <Wildfire className="m-auto w-8" />
       <strong>{info.title}</strong>
       <ul>
         <li>
